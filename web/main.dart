c1665de83c2eb9eb9c2d9d5a63c58d27e6c0d06a
Tal <mlcoch.zdenek@seznam.cz>
2016-02-19 17:09:33 +0100

    Gamee example project

--- web/main.dart
+++ web/main.dart
@@ -1,8 +1,29 @@
-// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// Copyright (c) 2016, Zdeněk Mlčoch. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'dart:html';
+import 'dart:js';
+import 'package:gamee/gamee.dart';
+
+CanvasElement canvas;
+CanvasRenderingContext2D ctx;
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  canvas = querySelector("canvas");
+  ctx = canvas.context2D;
+  Gamee gamee = new Gamee(context[r"$gameeNative"]);
+
+  gamee.onRestart.add((){
+    print("restarted");
+  });
+
+  gamee.onPausedChange.add((){
+    print("paused ${gamee.paused}");
+  });
+
+  drawSomething();
+}
+
+void drawSomething(){
+  ctx.fillRect(10,10,10,10);
 }
